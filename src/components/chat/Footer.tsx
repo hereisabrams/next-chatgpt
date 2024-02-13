2c40e6c3a73680f63bd1d2394bbce020ce76a787
Joao <joao.airesmatos@gmail.com>
2024-02-13 19:48:08 +0000

    Fix

--- src/components/chat/Footer.tsx
+++ src/components/chat/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = ({children}: ChildrenProps) => {
     return (
-        <div className="relative my-4 lg:my-6 h-[20%]">
+        <div className="relative h-[20%]">
             <footer className="relative max-w-4xl mx-auto px-3 lg:px-0">
                 {children}
             </footer>
